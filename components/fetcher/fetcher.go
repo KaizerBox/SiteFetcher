fa753aff3e7e14b5b594f21581dbc71d4d983171
KaizerBox <shawnlidsofficial@gmail.com>
2026-05-15 15:12:03 -0600

    Refactor Fetcher
    Refactor Fetcher structure to be combine Client, Request Builder, and ResponseParser component.

--- components/fetcher/fetcher.go
+++ components/fetcher/fetcher.go
@@ -8,16 +8,20 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
+
+
+type Fetcher struct {
+	Client *http.Client //Overall Client shared among the sites, manage content, share connections etc.
+	RequestBuilder *RequestBuilder //Per Site, each site implements this to customize header, auth, query params, etc Sents the query, and returns the Response Result or error
+	ReponseParser *ReponseParser //Per Site, each site implements this to parse the Response Result from RequestBuilder's call to website. 
+	                             //Uses the response values to determine if the condition for this item is satisfied, if it is then call a function on the Reponse Result to generate a email template 
+								 //struct that can be passed to gmailer to send.
 
 type FetcherSetting struct {
 	MaxFetchTimeLimit int
-}
-
-type Fetcher struct {
-	FetchContext context.Context
-	Client       *http.Client
-	FetcherSetting
+	FetcherSiteName string
 }
 
 type FetchTimeOutError struct {
@@ -37,18 +41,48 @@ func (e *FetchResponseStatusError) Error() string {
 	return fmt.Sprintf("Server response unexpected: StatusCode: %d, Message: %s", e.StatusCode, e.StatusMessage)
 }
 
+type RequestParameters struct {
+	requestContext context.Context
+	requestUrl string
+}
+
+func (r *RequestParameters) RequestParameters(rContext, rUrl) RequestParameters {
+	return RequestParameters{requestContext: rContext, requestUrl:rUrl}
+}
+
+func (r *requestParameters) GetRequestContext() (context.Context, error) {
+	return r.requestContext
+}
+
+func (r *requestParameters) GetRequestUrl() (string, error) {
+	if len(r.requestURL) == nil {
+		return "", errors.New("Error - Empty URL was provided for Request URL.")
+	}
+}
+
 // Use a single http.Client to improve performance. Maintaning keep alive can avoid extra tcp handshakes
 // Can look to tune the http.Transport as well, such as IdleConnTimeout, MaxIdleConns, MaxIdleConnsPerHost, etc
-func (f *Fetcher) FetchWithUrl(url string) ([]byte, error) {
+func (f *Fetcher) FetchWithUrl(requestParams RequestParameters) ([]byte, time.Time, error) {
+	reqStartTime = time.Now()
+	
+	reqContext, err := requestParams.GetRequestContext()
+	if err != nil {
+		return nil, reqStartTime, err
+	}
 
-	req, err := http.NewRequestWithContext(f.FetchContext, http.MethodGet, url, nil)
+	reqUrl, err := requestParams.GetRequestUrl()
+	if err != nil {
+		return nil, reqStartTime, err
+	}
+	
+	req, err := http.NewRequestWithContext(reqContext, http.MethodGet, reqUrl, nil)
 
 	if errors.Is(err, context.DeadlineExceeded) {
-		return nil, &FetchTimeOutError{
+		return nil, reqStartTime, &FetchTimeOutError{
 			FailedMaxFetchTimeLimit: f.MaxFetchTimeLimit, //TODO
 		}
 	} else if err != nil {
-		return nil, err
+		return nil, reqStartTime, err
 	}
 
 	//req.Header.Add()
@@ -60,7 +94,7 @@ func (f *Fetcher) FetchWithUrl(url string) ([]byte, error) {
 			FailedMaxFetchTimeLimit: f.MaxFetchTimeLimit, //TODO
 		}
 	} else if err != nil {
-		return nil, err
+		return nil, reqStartTime, err
 	}
 	//defer after checking for error to avoid Body.Close() error which may occur when Do fails
 	defer resp.Body.Close()
@@ -69,10 +103,10 @@ func (f *Fetcher) FetchWithUrl(url string) ([]byte, error) {
 		errMessage, readErrBodyErr := io.ReadAll(resp.Body)
 
 		if readErrBodyErr != nil {
-			return nil, readErrBodyErr
+			return nil, reqStartTime, readErrBodyErr
 		}
 
-		return nil, &FetchResponseStatusError{
+		return nil, reqStartTime, &FetchResponseStatusError{
 			StatusCode:    resp.StatusCode,
 			StatusMessage: errMessage,
 		}
@@ -80,9 +114,50 @@ func (f *Fetcher) FetchWithUrl(url string) ([]byte, error) {
 
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, reqStartTime, err
 	}
 
-	return respBody, nil
+	return respBody, reqStartTime, nil
 
+}
+
+type ParseResponseBodyResult struct {
+	ItemName string
+	OriginalPrice float32
+	DiscountAmount float32
+	PriceAfterDiscount float32
+	Stock int
+	QueryExecutionTimestamp time.Time
+	QueryURL string
+}
+
+//func (p *ParseResponseBodyResult) GenerateEmailTemplate() {
+// --TODO Generates the necessary string fields gmailer to send email to user when query condition of the ResponseBody as parsed sucessfully.
+//}
+
+//Parse the ResponseBody based on query conditions (i.e. price, stock, etc).
+//Might need to implement a sub function for each condition whic reuslt in boolean, then ParseResponseMessage evaluates the overall condition based on the sub functions.
+func (f *Fetcher) ParseResponseMessage(reponseBody []byte) (*ParseResponseBodyResult, error) {
+	return nil, error
+}
+
+//Contains all the conditions for the item, each item have this struct. Site and URL then uniquely identifies each tracker.
+type ItemQueryCondition struct {
+	QueryURL string
+	PriceLowerBound float32
+	PriceUpperBound float32
+	InStockStatus bool
+	StockLowerBound int
+	StockUpperBound int
+	DiscountStatus bool
+	DiscountLowerBound float32
+	DiscountUpperBound float32
+}
+
+func (i *ItemQueryCondition) ItemQueryStatus() (ItemQueryConditions, error) {
+	//creates tis based on json or other structured file format parsed for the file that stores this info
+}
+
+func (i *ItemQueryCondition) EvalResponsePriceCondition(*ParseResponseBodyResult, price) (bool) {
+	
 }
